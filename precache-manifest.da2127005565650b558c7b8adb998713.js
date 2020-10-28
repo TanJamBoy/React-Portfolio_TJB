e17b0b0f7469a2adb302dee3619812de3619a93c
Tanner Boyer <tannerjboyer@gmail.com>
2020-10-28 06:42:13 -0700

    Updates

---
+++ precache-manifest.da2127005565650b558c7b8adb998713.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60f2ab7da934e43cea9b1a7cc574ccb5",
+    "url": "/React-Portfolio_TJB/index.html"
+  },
+  {
+    "revision": "ca1adcc07c28687b8d30",
+    "url": "/React-Portfolio_TJB/static/css/main.c1af2374.chunk.css"
+  },
+  {
+    "revision": "c3766224b5d7ea301d01",
+    "url": "/React-Portfolio_TJB/static/js/2.560c094b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/React-Portfolio_TJB/static/js/2.560c094b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ca1adcc07c28687b8d30",
+    "url": "/React-Portfolio_TJB/static/js/main.33f82fc8.chunk.js"
+  },
+  {
+    "revision": "266d7dfc33c97d95a440",
+    "url": "/React-Portfolio_TJB/static/js/runtime-main.1d1f18d1.js"
+  }
+]);
