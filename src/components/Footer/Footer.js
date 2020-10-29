eb1af1cb1a71f96410c9f55d1c458a510229b0c1
Tanner Boyer <tannerjboyer@gmail.com>
2020-10-28 17:52:49 -0700

    fixing deploy

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -10,10 +10,10 @@ function Footer() {
                             <img src="https://icons-for-free.com/iconfiles/png/512/linked+linkedin+logo+social+icon-1320191784782940875.png" width="50px" alt="Linkedin Logo"/>
                         </a>
                         <a className="mr-5" href="https://github.com/TanJamBoy" target="_blank" rel="noopener noreferrer">
-                            <img src="/githubLogo.png" width="50px" alt="Github Logo"/>
+                            <img src="./githubLogo.png" width="50px" alt="Github Logo"/>
                         </a>
                         <a href="https://docs.google.com/document/d/1FFpHSK37EeIWwld85VcNH-uPRkcnKCHm1ZcnKZVyEmo/preview" target="_blank" rel="noopener noreferrer">
-                            <img src="/resumeLogo.png" width="50px" alt="Github Logo"/>
+                            <img src="./resumeLogo.png" width="50px" alt="Resume Logo"/>
                         </a>
                     </center>
                 </div>
