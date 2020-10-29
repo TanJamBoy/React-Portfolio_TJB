ab9aa9ae4623620e33c1811fd06cb049350040fc
Tanner Boyer <tannerjboyer@gmail.com>
2020-10-28 18:23:02 -0700

    Updates

--- precache-manifest.815b2b7272b44091fbd4858d74a72a6c.js
+++ precache-manifest.815b2b7272b44091fbd4858d74a72a6c.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "a7e8dcb45e32f06fc30c35565fffec0d",
+    "revision": "0471e01a8ba3554cff3d21ef8c3cca46",
     "url": "/index.html"
   },
   {
-    "revision": "8b4d97b2f0d2c02adfb9",
+    "revision": "be36a160658a1278e687",
     "url": "/static/css/main.c1af2374.chunk.css"
   },
   {
-    "revision": "c3766224b5d7ea301d01",
-    "url": "/static/js/2.560c094b.chunk.js"
+    "revision": "eb73fcce252d08bd0ff1",
+    "url": "/static/js/2.51ca2063.chunk.js"
   },
   {
     "revision": "c64c486544348f10a6d6c716950bc223",
-    "url": "/static/js/2.560c094b.chunk.js.LICENSE.txt"
+    "url": "/static/js/2.51ca2063.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "8b4d97b2f0d2c02adfb9",
-    "url": "/static/js/main.a6c5470b.chunk.js"
+    "revision": "be36a160658a1278e687",
+    "url": "/static/js/main.93641085.chunk.js"
   },
   {
     "revision": "4f2b1704eab49d50853b",
